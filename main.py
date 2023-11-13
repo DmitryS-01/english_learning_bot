f2359ba1bb29e0cefe95a86b405c33d71f45d36b
Dmitry S <122632430+Legendarochka123@users.noreply.github.com>
2023-11-14 00:00:07 +0400

    версия 1.0 теперь бот может общаться в тг, сохраняет историю в отдельные файлы и минимально функционирует

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import asyncio
 import logging
 import sys
+import os
 
 
 from config import bot_token
+from message_processing import gpt_response_creation
 
 
 from aiogram import Bot, Dispatcher, types
@@ -22,9 +24,54 @@ async def command_start_handler(message: Message) -> None:
 
 @dp.message()
 async def lame_message_handler(message: types.Message) -> None:
+
+    # id пользователя и текст его сообщения
+    user_message = message.text
+    user_id = message.chat.id
+
     try:
-        bot_answer = 'TEST BROOOOOOOO'
-        await message.answer(bot_answer)
+        # расположение файла с иторией общения с данным персонажем
+        program_dir = os.path.dirname(__file__)
+        messages_file = str(os.path.join(program_dir, f'{user_id}.txt'))
+
+        # если этого файла нет, создаем его
+        try:
+            with open(messages_file, 'x', encoding='utf-8'):
+                pass
+        except Exception as e:
+            if '[Errno 17] File exists: ' not in str(e):
+                print(f'Ошибка при обращении к файлам программы 1 (ошибку вызвал петух на {user_id})')
+
+        # достаем 10 последних сообщений
+        last_messages = list()
+        try:
+            with open(messages_file, encoding='utf-8') as f:
+                last_10_messages = f.readlines()[-20:]
+            for pos in range(0, len(last_10_messages), 2):
+                last_messages += [{'role': last_10_messages[pos].strip(), 'content': last_10_messages[pos + 1].strip()}]
+        except Exception as e:
+            if '[Errno 17] File exists: ' not in str(e):
+                print(f'Ошибка при обращении к файлам программы 2 (ошибку вызвал петух на {user_id})')
+        # добавляем к ним текущее сообщение этого петушары
+        last_messages = (last_messages + [{'role': 'user', 'content': user_message}])[-20:]
+
+        try:
+            await message.answer('Please wait . . .')
+
+            # получаем ответ от модели
+            bot_answer = await gpt_response_creation(last_messages)
+
+            # дописываем историю
+            with open(messages_file, 'a', encoding='utf-8') as f:
+                print('user', user_message.replace('\n', '\t'), sep='\n', file=f)
+                print('assistant', bot_answer.replace('\n', '\t'), sep='\n', file=f)
+
+            # отвечаем этому пидрилле
+            await message.answer(f'🤖 > {bot_answer}')
+
+            # прорабатываем упущения
+        except Exception as e:
+            print(f'Ошибка {e} ! (ошибку вызвал петух на {user_id}')
     except Exception as e:
         print(f"тварь на {message.from_user.id} / {message.from_user.full_name} че то сломала")
 
--- message_processing.py
+++ message_processing.py
@@ -1,13 +1,12 @@
-import openai
-import os
+from openai import AsyncOpenAI
 from config import open_ai_api_key
 
-openai.api_key = open_ai_api_key
+client = AsyncOpenAI(api_key=open_ai_api_key)
 
 
 # ответ гпт
-def gpt_response_creation(context):
-    response = openai.ChatCompletion.create(  # создание ответа
+async def gpt_response_creation(context):
+    response = await client.chat.completions.create(  # создание ответа
         model='gpt-3.5-turbo-16k',  # модель
         # запрос + история сообщений
         messages=[{'role': 'system', 'content': "You are a bot that helps people learn English. You have access to the "
@@ -21,70 +20,3 @@ def gpt_response_creation(context):
         frequency_penalty=0.25,  # уменьшает вероятность модели повторить одну и ту же строку дословно
     )
     return response.choices[0].message.content
-
-
-# принимаем сообщения, отвечаем на них
-def chat(user_message, user_id):
-    program_dir = os.path.dirname(__file__)
-    messages_file = str(os.path.join(program_dir, f'{user_id}.txt'))
-
-    try:
-        with open(messages_file, 'x', encoding='utf-8'):
-            pass
-    except Exception as e:
-        if '[Errno 17] File exists: ' not in str(e):
-            print(f'Ошибка при обращении к файлам программы 1 (ошибку вызвал петух на {user_id}')
-
-
-    LAST_MESSAGES = list()
-    try:
-        with open(messages_file, encoding='utf-8') as f:
-            last_10_messages = f.readlines()[-20:]
-        for pos in range(0, len(last_10_messages), 2):
-            LAST_MESSAGES += [{'role': last_10_messages[pos].strip(), 'content': last_10_messages[pos + 1].strip()}]
-    except Exception as e:
-        if '[Errno 17] File exists: ' not in str(e):
-            print(f'Ошибка при обращении к файлам программы 2 (ошибку вызвал петух на {user_id}')
-
-    LAST_MESSAGES = (LAST_MESSAGES + [{'role': 'user', 'content': user_message}])[-20:]
-    try:
-        print('Please wait . . .')
-        bot_message = gpt_response_creation(LAST_MESSAGES)
-        with open(messages_file, 'a', encoding='utf-8') as f:
-            print('user', user_message.replace('\n', '\t'), sep='\n', file=f)
-            print('assistant', bot_message.replace('\n', '\t'), sep='\n', file=f)
-        print(f'🤖 > {bot_message}')
-    except Exception as e:
-        print(f'Ошибка {e} ! (ошибку вызвал петух на {user_id}')
-
-
-# создаем файл для истории сообщений
-program_dir = os.path.dirname(__file__)
-messages_file = str(os.path.join(program_dir, 'messages.txt'))
-try:
-    with open(messages_file, 'x', encoding='utf-8'):
-        pass
-except Exception as e:
-    if '[Errno 17] File exists: ' not in str(e):
-        print('Ошибка при обращении к файлам программы 1')
-
-# достаем все че надо
-
-LAST_MESSAGES = list()
-try:
-    with open(messages_file, encoding='utf-8') as f:
-        last_10_messages = f.readlines()[-20:]
-    for pos in range(0, len(last_10_messages), 2):
-        LAST_MESSAGES += [{'role': last_10_messages[pos].strip(), 'content': last_10_messages[pos + 1].strip()}]
-except Exception as e:
-    if '[Errno 17] File exists: ' not in str(e):
-        print('Ошибка при обращении к файлам программы 2')
-
-# запуск
-print('''=====================================================================
-👋 Привет! Только этой программе на тебя не все равно, излей душу! ❤
-"STOP" = конец; старайтесь не спамить сообщениями
-=====================================================================
-''')
-if __name__ == '__main__':
-    chat()
