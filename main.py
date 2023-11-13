bbb510e06ec8a705d06481cea5bdaf3ec2fae77c
Dmitry S <122632430+Legendarochka123@users.noreply.github.com>
2023-11-14 00:21:27 +0400

    версия 1.2 добавлено время ожидания между обращениями к апи openai, теперь бот именно ОТВЕЧАЕТ на сообщения, а не просто пишет в чат

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import asyncio
 import logging
 import sys
 import os
+from datetime import datetime, timedelta
 
-
-from config import bot_token
+from config import bot_token, delta_time  # delta_time - задержка между некст обращением к OpenAI
 from message_processing import gpt_response_creation
 
 
@@ -14,8 +14,14 @@ from aiogram.filters import CommandStart
 from aiogram.types import Message
 from aiogram.utils.markdown import hbold
 
+
+
 dp = Dispatcher()
 
+# очередь на отправку запроса в OpenAI
+queue = tuple()
+# номер сообщения
+num = 1
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
@@ -24,6 +30,8 @@ async def command_start_handler(message: Message) -> None:
 
 @dp.message()
 async def lame_message_handler(message: types.Message) -> None:
+
+    global num, queue, last_use_time
 
     # id пользователя и текст его сообщения
     user_message = message.text
@@ -54,9 +62,15 @@ async def lame_message_handler(message: types.Message) -> None:
         # добавляем к ним текущее сообщение этого петушары
         last_messages = (last_messages + [{'role': 'user', 'content': user_message}])[-20:]
 
+        # встали в очередь
+        pos, num = num, num + 1
+        queue += (pos,)
         try:
             await message.answer('Please wait . . .')
 
+            # ожидание очереди
+            while queue.index(pos) != 0 or last_use_time + timedelta(seconds=delta_time) > datetime.utcnow():
+                await asyncio.sleep(1)
             # получаем ответ от модели
             bot_answer = await gpt_response_creation(last_messages)
 
@@ -66,11 +80,14 @@ async def lame_message_handler(message: types.Message) -> None:
                 print('assistant', bot_answer.replace('\n', '\t'), sep='\n', file=f)
 
             # отвечаем этому пидрилле
-            await message.answer(f'🤖 > {bot_answer}')
+            await message.reply(f'🤖 > {bot_answer}')
 
             # прорабатываем упущения
         except Exception as e:
             print(f'Ошибка {e} ! (ошибку вызвал петух на {user_id}')
+        finally:
+            # двигаем очередь
+            queue, last_use_time = queue[1:], datetime.utcnow()
     except Exception as e:
         print(f"тварь на {message.from_user.id} / {message.from_user.full_name} че то сломала")
 
@@ -88,5 +105,8 @@ if __name__ == "__main__":
         os.mkdir("dialogues")
     program_dir = os.path.dirname(__file__)
     dialogues_folder = os.path.join(program_dir, "dialogues")
+
+    # запоминаем время запуска проги, чтоб просчитывать, когда можно будет отправить запрос
+    last_use_time = datetime.utcnow()
 
     asyncio.run(main())
